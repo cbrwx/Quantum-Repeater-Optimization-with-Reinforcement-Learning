170b2957b286f45db108e87751fcaba29dd22ee2
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-05-27 21:02:52 +0200

    Update QRepeater_reinforced.py
    errors, fixes and such

--- QRepeater_reinforced.py
+++ QRepeater_reinforced.py
@@ -1,38 +1,54 @@
 import gym
 import qiskit.quantum_info as qi
-from stable_baselines3 import PPO
+import numpy as np
+import os
+import uuid
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
 from qiskit.quantum_info import random_unitary
-import numpy as np
-import uuid
-import os
-
+from qiskit.circuit.library import HGate, XGate, ZGate
+from stable_baselines3 import PPO
 
 class QuantumRepeaterEnv(gym.Env):
     def __init__(self, paths):
         super(QuantumRepeaterEnv, self).__init__()
 
-        # Define action and observation space
-        self.action_space = gym.spaces.Discrete(2)  # Choices for error correction (0) and entanglement purification (1)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)  # Measurements of quantum channel conditions
+        if not self.valid_graph(paths):
+            raise ValueError("The input paths are not a valid graph with appropriate weights")
+        
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
 
         self.qc = self.initialize_quantum_circuit()
         self.paths = paths
 
+    @staticmethod
+    def valid_graph(paths):
+        if not paths:
+            return False
+        for p in paths.values():
+            if not p:
+                return False
+            for weight in p.values():
+                if weight <= 0:
+                    return False
+        return True
+
     def step(self, action):
+        if action not in [0,1]:
+            raise ValueError(f"Illegal action: {action}. Action must be either 0 (error correction) or 1 (entanglement purification).")
+
         if action == 0:
             self.apply_error_correction()
         else:
             self.apply_entanglement_purification()
 
         observation = self.measure_channel_conditions()
-        reward = self.measure_transmission_fidelity()  # Reward is based on successful quantum transmission
-        done = reward >= 0.95  # Set the done condition when the fidelity reaches 0.95 or above
+        reward = self.measure_transmission_fidelity()
+        done = reward >= 0.95
 
         return observation, reward, done, {}
 
     def reset(self):
-        # Reset the state of the environment to an initial state
         self.qc = self.initialize_quantum_circuit()
         self.reset_quantum_channel()
 
@@ -40,7 +56,6 @@ class QuantumRepeaterEnv(gym.Env):
         return observation
 
     def initialize_quantum_circuit(self):
-        # Initialize the quantum circuit
         qreg = QuantumRegister(6, name="q")
         cregz = ClassicalRegister(1, name="cregz")
         cregx = ClassicalRegister(1, name="cregx")
@@ -49,7 +64,6 @@ class QuantumRepeaterEnv(gym.Env):
         return qc
 
     def apply_error_correction(self):
-        # Apply error correction logic here
         error_correction_crs = [ClassicalRegister(1, name=f"cr_{i}") for i in range(1, 4)]
         self.qc.add_register(*error_correction_crs)
         syndrome_meas = ClassicalRegister(2, name="syndrome_meas")
@@ -57,14 +71,12 @@ class QuantumRepeaterEnv(gym.Env):
 
         qubit_trios = self.qc.qubits[:3]
         error_corrected_reg = error_correction(self.qc, qubit_trios, [self.qc.cregs[0]] * len(qubit_trios))
-        
+
     def apply_entanglement_purification(self):
-        # Apply entanglement purification logic here
         qubit_pair = [self.qc.qubits[0], self.qc.qubits[1]]
         entanglement_purification(self.qc, qubit_pair[0], qubit_pair[1], self.qc.cregs[-1])
 
     def measure_channel_conditions(self):
-        # Measure the channel conditions and normalize
         self.qc.measure(self.qc.qubits[0], self.qc.cregs[0])
         self.qc.measure(self.qc.qubits[1], self.qc.cregs[1])
 
@@ -77,24 +89,20 @@ class QuantumRepeaterEnv(gym.Env):
         return channel_conditions
 
     def measure_transmission_fidelity(self):
-        # Measure the transmission fidelity, could be represented by error rate
         self.qc.measure(self.qc.qubits[2], self.qc.cregs[2])
 
         backend = Aer.get_backend('qasm_simulator')
         result = execute(self.qc, backend, shots=1000).result()
         counts = result.get_counts()
 
-        fidelity = counts.get('000', 0) / sum(counts.values())
-        return fidelity
+        fidelity = sum(counts.values()) - counts.get('000', 0)
+        fidelity /= sum(counts.values())
+        return 1 - fidelity
 
     def reset_quantum_channel(self):
-        # Reset the Quantum Channel
         self.qc.reset(self.qc.qubits)
-
-    def multiplexing(self, channels):
-        num_qubits = self.qc.num_qubits // len(channels)
-        for i in range(0, self.qc.num_qubits, num_qubits):
-            create_Bell_pair(self.qc, channels[i], channels[i + 1])
+        for creg in self.qc.cregs:
+            creg.reset()
 
     def dijkstra_shortest_path(self, source, destination):
         paths = self.paths
@@ -102,7 +110,7 @@ class QuantumRepeaterEnv(gym.Env):
         previous_vertices = {vertex: None for vertex in paths}
         dist[source] = 0
         vertices = paths.copy()
-        
+
         while vertices:
             current_vertex = min(vertices, key=lambda vertex: dist[vertex])
             vertices.remove(current_vertex)
@@ -122,56 +130,16 @@ class QuantumRepeaterEnv(gym.Env):
 
         return path[::-1]
 
-# Graph with nodes as qubit positions and edges as path cost (lower is better)
-paths = {
-    0: {1: 5, 2: 10},
-    1: {3: 10},
-    2: {4: 10},
-    3: {5: 5},
-    4: {5: 5}
-}
-
-source = 0
-destination = 5
-num_repeater_stations = 3
-
-# Initialize the environment and the agent
-env = QuantumRepeaterEnv(paths)
-
-# Check if pre-existing model exists, if so, load it
-if os.path.isfile("current_quantum_repeater_model.zip"):
-    model = PPO.load("current_quantum_repeater_model")
-else:
-    model = PPO("MlpPolicy", env, verbose=1)
-
-# Define best reward and the evaluation interval
-best_reward = -np.inf
-evaluation_interval = 1000  # Evaluate the model every 1000 steps
-total_timesteps = 10000  # Total number of steps for training
-
-for timestep in range(0, total_timesteps, evaluation_interval):
-    # Train the agent
-    model.learn(total_timesteps=evaluation_interval)
-
-    # Save the current model
-    model.save("current_quantum_repeater_model")
-
-    # Evaluate the model and update the best reward
-    avg_reward = np.mean([model.evaluate_policy(env, deterministic=True)[0] for _ in range(10)])
-    if avg_reward > best_reward:
-        best_reward = avg_reward
-        model.save("best_quantum_repeater_model")
-
-# Load the best model
-model = PPO.load("best_quantum_repeater_model")
-
-# Use the trained agent to operate the quantum repeater
-observation = env.reset()
-while True:
-    action, _states = model.predict(observation, deterministic=True)
-    observation, reward, done, info = env.step(action)
-    if done:
-        break
+def error_correction_circuit(num_repeater_stations):
+    circuits = []
+    for i in range(num_repeater_stations):
+        qreg = QuantumRegister(6, name=f"q_{i}")
+        cregz = ClassicalRegister(1, name=f"cregz_{i}")
+        cregx = ClassicalRegister(1, name=f"cregx_{i}")
+        creg_same = ClassicalRegister(1, name=f"cr_same_{i}")
+        qc = QuantumCircuit(qreg, cregz, cregx, creg_same)
+        circuits.append(qc)
+    return circuits
 
 def create_Bell_pair(qc, a, b): 
     qc.h(a)
@@ -202,8 +170,7 @@ def entanglement_purification(qc, a, b, cr_same):
 def quantum_memory(qc, a, duration):
     qc.append_gate("DoubleGate", [a], params=[duration])
 
-def error_correction(qc, a, crs):  # Implement error correction codes
-    # Three-qubit error correction code, implement more complex codes as needed
+def error_correction(qc, a, crs):
     num_qubits = len(a)
     error_corrected_reg = QuantumRegister(num_qubits, name="error_corrected")
     qc.add_register(error_corrected_reg)
@@ -224,15 +191,14 @@ def error_correction(qc, a, crs):  # Implement error correction codes
 
 def synchronization(qc, ops, relative_clock):
     current_timestamp = 0
-    
+
     for clock_event in relative_clock:
         op, qubits, event_timestamp = clock_event
         if current_timestamp < event_timestamp:
             delay_duration = event_timestamp - current_timestamp
-            qc.delay(delay_duration, qubits, unit='dt')  # Add delay to maintain synchronization
+            qc.delay(delay_duration, qubits, unit='dt')
             current_timestamp = event_timestamp
 
-        # If the operation is in the list of qubit operations, apply it to the qubits
         if op in ops:
             qc.append(op(), qubits)
 
@@ -241,37 +207,66 @@ def multiplexing(qc, channels, names=None):
     for i in range(0, qc.num_qubits, num_qubits):
         create_Bell_pair(qc, channels[i], channels[i + 1])
 
-def error_correction_circuit(n_repeater_stations):
-    circuits = []
-    qubit_trios = [QuantumRegister(3, name=f"q_{i}") for i in range(n_repeater_stations)]
-    for idx, q_regs in enumerate(zip(*qubit_trios)):
-        qc = QuantumCircuit(sum(q_regs, QuantumRegister(0)))
-        qc.add_register(ClassicalRegister(1, name=f"cr_{idx}"))
-        qc.add_register(ClassicalRegister(2, name=f"syndrome_meas"))
-        error_corrected_reg = error_correction(qc, q_regs, [qc.cregs[idx]] * len(q_regs))
-        circuits.append(qc)
-    return circuits
+paths = {
+    0: {1: 5, 2: 10},
+    1: {3: 10},
+    2: {4: 10},
+    3: {5: 5},
+    4: {5: 5}
+}
 
 source = 0
 destination = 5
 num_repeater_stations = 3
 
-# Initializations
+env = QuantumRepeaterEnv(paths)
+
+if os.path.isfile("current_quantum_repeater_model.zip"):
+    model = PPO.load("current_quantum_repeater_model")
+else:
+    model = PPO("MlpPolicy", env, verbose=1)
+
+best_reward = -np.inf
+evaluation_interval = 1000
+total_timesteps = 10000
+
+for timestep in range(0, total_timesteps, evaluation_interval):
+    model.learn(total_timesteps=evaluation_interval)
+
+    model.save("current_quantum_repeater_model")
+
+    avg_reward = np.mean([model.evaluate_policy(env, deterministic=True)[0] for _ in range(10)])
+    if avg_reward > best_reward:
+        best_reward = avg_reward
+        model.save("best_quantum_repeater_model")
+
+model = PPO.load("best_quantum_repeater_model")
+
+observation = env.reset()
+while True:
+    action, _states = model.predict(observation, deterministic=True)
+    observation, reward, done, info = env.step(action)
+    if done:
+        break
+
+source = 0
+destination = 5
+num_repeater_stations = 3
+
 rep_circuits = error_correction_circuit(num_repeater_stations)
 shortest_path = env.dijkstra_shortest_path(source, destination)
+
 print(f"Shortest path: {shortest_path}")
 
 for idx, rep_circuit in enumerate(rep_circuits):
     qc = QuantumCircuit(rep_circuit.qregs[0], rep_circuit.qregs[1], ClassicalRegister(1, name=f"cregz_{idx}"), ClassicalRegister(1, name=f"cregx_{idx}"), ClassicalRegister(1, name=f"cr_same_{idx}"))
     qreg = rep_circuit.qregs[0]
 
-    # Initialize the state
     initial_state = qi.random_statevector(2)
     qc.initialize(initial_state.data, qreg[0])
 
-    # Quantum Repeater Workflow
     create_Bell_pair(qc, qreg[0], qreg[1])
-    quantum_memory(qc, qreg[0], 100)  # Store qubit 0 for 100 timesteps
+    quantum_memory(qc, qreg[0], 100)
     Bell_pair_teleportation(qc, qreg[0], qreg[1], qreg[2], qc.cregs[idx+1], qc.cregs[idx+2])
     entanglement_swapping(qc, qreg[2], qreg[3], qc.cregs[idx+1], qc.cregs[idx+2])
     qubit_trios_qregs = [qubit_trios[i].qubits for i in range(len(qubit_trios))]
@@ -280,16 +275,14 @@ for idx, rep_circuit in enumerate(rep_circuits):
     entanglement_purification(qc, qreg[0], qreg[1], qc.cregs[-1])
     create_Bell_pair(qc, qreg[4], qreg[5])
 
-    # Relative clock events in the form (operation, qubits, timestamp)
     relative_clock = [
         (HGate, [qreg[0]], 10),
         (XGate, [qreg[1]], 20),
         (ZGate, [qreg[2]], 30),
     ]
 
-    # Apply operations with synchronization
     synchronization(qc, [HGate, XGate, ZGate], relative_clock)
 
     backend = Aer.get_backend('qasm_simulator')
     result = execute(qc, backend, shots=1).result()
-    print(result.get_counts())
+    print(result.get_counts())       
